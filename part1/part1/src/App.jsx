fb5534ca77c73a31fef519867383a6cc218af504
hartonenolli <hartonenolli@gmail.com>
2025-07-23 22:22:39 +0300

    App.jsx Footer

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -2,12 +2,13 @@ const App = () => {
   const ika = 25;
   const nimi = "Jaska";
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Heikki" age={10+11}/>
       <Hello name={nimi} age={ika}/>
       <Hello name="Anni" age={33}/>
-    </div>
+      <Footer />
+    </>
   )
 }
 
@@ -16,6 +17,15 @@ const Hello = (props) => {
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/mluukkai">mluukkai</a>
     </div>
   )
 }
