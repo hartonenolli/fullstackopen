7bc316c36ee792b1b6afd4bcae11ae42f6fb63d1
hartonenolli <hartonenolli@gmail.com>
2025-07-25 13:41:39 +0300

    Destructuring part1

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -12,11 +12,15 @@ const App = () => {
   )
 }
 
-const Hello = (props) => {
-  console.log(props)
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
