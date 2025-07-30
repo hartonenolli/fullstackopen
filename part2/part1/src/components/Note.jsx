d160f09b4ccd473fd7668b3a226eb779979ea821
hartonenolli <hartonenolli@gmail.com>
2025-07-30 09:15:03 +0300

    Added services/notes.js

--- part2/part1/src/components/Note.jsx
+++ part2/part1/src/components/Note.jsx
@@ -1,5 +1,15 @@
-const Note = ({ note }) => {
-    return <li>{note.content}</li>
-  }
-  
-  export default Note
+const Note = ({ note, toggleImportance }) => {
+  // console.log('Note: ', note);
+  // console.log('ToggleImportance: ', toggleImportance);
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+  return (
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
+
+export default Note
