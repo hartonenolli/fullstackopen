e64b630449d732041b355a02d6886ecc5cdd6f14
hartonenolli <hartonenolli@gmail.com>
2025-07-30 09:34:06 +0300

    2.12: puhelinluettelo step7

--- part2/exercises2/puhelinluettelo/src/App.jsx
+++ part2/exercises2/puhelinluettelo/src/App.jsx
@@ -30,7 +30,11 @@ const App = () => {
     }
     const nameExist = names.includes(newName)
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(personObject))
+      : axios.post('http://localhost:3001/persons', personObject)
+          .then(response => {
+            console.log('response', response.data)
+            setPersons(persons.concat(response.data))
+          })
     setNewName('')
     setNewNumber('')
   }
